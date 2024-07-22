0cd3aaff92d3aa57d79cb775bc1b9bf4a5dfd8ad
San Kanao <sandeep.kanao@gmail.com>
2024-07-22 22:45:43 +0000

    WIP for Mask

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -170,6 +170,8 @@ export function SideBar(props: { className?: string }) {
           icon={<MaskIcon />}
           text={shouldNarrow ? undefined : Locale.Mask.Name}
           className={styles["sidebar-bar-button"]}
+          onClick={() => showToast(Locale.WIP)}
+          /*
           onClick={() => {
             if (config.dontShowMaskSplashScreen !== true) {
               navigate(Path.NewChat, { state: { fromHome: true } });
@@ -177,6 +179,7 @@ export function SideBar(props: { className?: string }) {
               navigate(Path.Masks, { state: { fromHome: true } });
             }
           }}
+            */
           shadow
         />
         <IconButton
